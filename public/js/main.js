c57de23b747cb894d547fa5c7a366d6ca50b4956
badkins78 <72710121+badkins78@users.noreply.github.com>
2022-03-22 12:13:51 -0500

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,56 @@
+const deleteText = document.querySelector('.fa-trash')
+const addText = document.querySelector('.fa-calendar-check-o')
+
+Array.from(deleteText).forEach((element) => {
+  element.addEventListener('click', deleteChore)
+})
+
+Array.from(addText).forEach((element) => {
+  element.addEventListener('click', addWeekly)
+})
+
+async function deleteChore(){
+  const cName = this.parentNode.childNodes[1].innerText
+  const wName = this.parentNode.childNodes[3].innerText
+  const sName = this.parentNode.childNodes[5].innerText
+  try{
+    const response = await fetch('deleteChore', {
+      method: 'delete',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'choreName': cName,
+        'workerName': wName,
+        'supervisorName': sName
+      })
+    })
+    const data = await response.json()
+    console.log(data)
+    location.reload()
+  }catch(err){
+    console.log(err)
+  }
+}
+
+async function addWeekly(){
+  const cName = this.parentNode.childNodes[1].innerText
+  const wName = this.parentNode.childNodes[3].innerText
+  const sName = this.parentNode.childNodes[5].innerText
+  const tWeekly = Number(this.parentNode.childNodes[7].innerText)
+  try{
+    const response = await fetch('addWeekly', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'choreName': cName,
+        'workerName': wName,
+        'supervisorName': sName,
+        'timesWeekly': tWeekly
+      })
+    })
+    const data = await response.json()
+    console.log(data)
+    location.reload()
+  }catch(err){
+    console.log(err)
+  }
+}
---
+++ server.js
@@ -0,0 +1,68 @@
+const { time } = require('console')
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 8000
+require ('dotenv').config()
+
+let db,
+    dbConnectionStr=process.env.DB_STRING,
+    dbName = 'todo'
+
+MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
+    .then(client => {
+      console.log(`connected to ${dbName} Database`)
+      db = client.db(dbName)
+    })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json)
+
+app.get('/', (request, response)=>{
+  db.collection('chores').find().toArray()
+  .then(data => {
+    response.render('index.ejs', {info: data})
+  })
+  .catch(error => console.error(error))
+})
+
+app.post('addChore', (request, response) => {
+  db.collection('chores').insertOne({choreName: request.body.choreName,
+  workerName: request.body.workerName, supervisorName: request.body.supervisorName, timesWeekly: request.body.timesWeekly})
+  .then(result => {
+    console.log('chore added')
+    response.redirect('/')
+  })
+  .catch(error => console.error(error))
+})
+
+app.put('/updateTimes', (request, response)=>{
+  db.collection('chores').updateOne({choreName: request.body.choreName, workerName: request.body.workerName, supervisorName: request.body.supervisorName, timesWeekly: request.body.timesWeekly},{
+    $set:{
+      timesWeekly:request.body.timesWeekly + 1
+    },
+  },{
+      sort: {_id: -1},
+      upsert: true
+    })
+    .then(result => {
+      console.log('added instance')
+      response.json('instance added')
+    })
+    .catch(error => console.error(error))
+  })
+
+  app.delete('/deleteChore', (request, response) => {
+    db.collection('chores').deleteOne({choreName: request.body.chorename})
+    .then(result =>{
+      console.log('deleted chore')
+      response.json('deleted chore')
+    })
+    .catch(error => console.error(error))
+  })
+
+  app.listen(process.env.PORT || PORT , () => {
+    console.log(`server is running on port ${PORT}`)
+  })
