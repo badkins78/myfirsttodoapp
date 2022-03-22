e0ebc76c25a0ebf2ae20f6a8cf44835068f89432
badkins78 <72710121+badkins78@users.noreply.github.com>
2022-03-22 17:00:21 -0500

    changes

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
-const { time } = require('console')
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 5500
+require('dotenv').config()
 
 
 let db,
-
-    dbName = 'myfirsttodoappp'
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'todoapp'
 
 MongoClient.connect('mongodb+srv://bill:a1s2d3@cluster0.wwpd7.mongodb.net/todoapp?retryWrites=true&w=majority', {useUnifiedTopology: true})
     .then(client => {
       console.log(`connected to ${dbName} Database`)
       db = client.db(dbName)
     })
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
-app.use(express.json)
+app.use(express.json())
+
 
 app.get('/', (request, response)=>{
-  db.collection('chores').find().toArray()
+  db.collection('todoapp').find().sort({likes: -1}).toArray()
   .then(data => {
     response.render('index.ejs', {info: data})
   })
   .catch(error => console.error(error))
 })
 
-app.post('addChore', (request, response) => {
-  db.collection('chores').insertOne({choreName: request.body.choreName,
-  workerName: request.body.workerName, supervisorName: request.body.supervisorName, timesWeekly: request.body.timesWeekly})
+app.post('/addChore', (request, response) => {
+  db.collection('todoapp').insertOne({choreName: request.body.choreName,
+  workerName: request.body.workerName, supervisorName: request.body.supervisorName, timesWeekly: 1})
   .then(result => {
     console.log('chore added')
     response.redirect('/')
@@ -38,7 +40,7 @@ app.post('addChore', (request, response) => {
 })
 
 app.put('/updateTimes', (request, response)=>{
-  db.collection('chores').updateOne({choreName: request.body.choreName, workerName: request.body.workerName, supervisorName: request.body.supervisorName, timesWeekly: request.body.timesWeekly},{
+  db.collection('todoapp').updateOne({choreName: request.body.choreName, workerName: request.body.workerName, supervisorName: request.body.supervisorName, timesWeekly: request.body.timesWeekly},{
     $set:{
       timesWeekly:request.body.timesWeekly + 1
     },
@@ -54,7 +56,7 @@ app.put('/updateTimes', (request, response)=>{
   })
 
   app.delete('/deleteChore', (request, response) => {
-    db.collection('chores').deleteOne({choreName: request.body.chorename})
+    db.collection('todoapp').deleteOne({choreName: request.body.chorename})
     .then(result =>{
       console.log('deleted chore')
       response.json('deleted chore')
